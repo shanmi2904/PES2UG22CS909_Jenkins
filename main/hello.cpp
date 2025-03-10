db4f1b0d1151a56c0dd897f2a6b77476f0ac2e26
shanmi2904 <148444675+shanmi2904@users.noreply.github.com>
2025-03-10 17:06:36 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,11 +1,8 @@
-//File: hello.cpp
-//webhook_test_change
 #include <iostream>
 using namespace std;
 
-int main(void) {
-  cout << "Hello, World_PES2UG22CS909\n" //missing semicolon
-  cout << "Hello, Jenkins_PES2UG22CS909\n";
-  cout << "I have successfully built and run this  \n";
-  return 0;
+int main() {
+    cout << "Hello from PES2UG22CS909!" << endl  // Missing semicolon
+    return 0;
 }
+
