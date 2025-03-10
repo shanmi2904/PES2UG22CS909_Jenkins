278c0678eab3335251c7da874eb02a58fe51d64a
shanmi2904 <148444675+shanmi2904@users.noreply.github.com>
2025-03-10 17:05:08 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World_PES2UG22CS909\n";
+  cout << "Hello, World_PES2UG22CS909\n" //missing semicolon
   cout << "Hello, Jenkins_PES2UG22CS909\n";
   cout << "I have successfully built and run this  \n";
   return 0;
