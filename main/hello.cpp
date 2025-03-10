7d20db2338a3410be009de53f9c1ef9279680b7e
shanmi2904 <148444675+shanmi2904@users.noreply.github.com>
2025-03-10 18:24:06 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, Jenkins Pipeline!" << endl  // Missing semicolon (compilation error)
+    cout << "Hello, Jenkins Pipeline!" << 
     return 0;
 }
 
